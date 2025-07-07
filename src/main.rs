6c01f467cc6d9fade630517d7009865f6281aaaf
AndersonEngenheiro <pfanderson3@gmail.com>
2025-07-07 14:32:48 -0400

    feat: access email

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use headless_chrome::{Browser, LaunchOptions};
 use std::io; // Para ler a entrada do usuário
+use std::time::Duration;
 
-
-fn main() {
+fn uol() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let options = LaunchOptions {
         headless: false,
         
@@ -21,9 +21,17 @@ fn main() {
     tab.navigate_to("https://www.uol.com.br/").unwrap();
     tab.wait_until_navigated().unwrap();
 
+    tab.wait_for_element (".email__link.email__link--loggedOut")?.click()?;
+    tab.wait_for_element_with_custom_timeout(".email__link.email__link--loggedOut", Duration::from_millis(2000));
+    tab.type_str("leandroribeiro1999@yahoo.com")?.press_key("Enter")?;
 
-        // Esperar pela entrada do usuário
-        println!("Pressione Enter para fechar o navegador...");
-        let _ = io::stdin().read_line(&mut String::new()).unwrap(); // Aguarda até o usuário pressionar Enter
+    // Esperar pela entrada do usuário
+    println!("Pressione Enter para fechar o navegador...");
+    let _ = io::stdin().read_line(&mut String::new()).unwrap(); // Aguarda até o usuário pressionar Enter
 
+    Ok(())
+}
+
+fn main(){
+    uol();
 }
